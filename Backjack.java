e3957273266eaf349d23c0a9f4d4792dfbb31cc4
Colcolat <jj.zapatabuenfil@gmail.com>
2025-10-08 19:25:24 -0600

    Update Backjack.java
    El primer prototipo completo del Black Jack.
    
    Falta agregar:
    1- El croupier
    2- Cartas de figuras
    3- Un sistema de presupuesto y apuestas

--- Backjack.java
+++ Backjack.java
@@ -7,6 +7,8 @@ public class Backjack {
         //Variables
         int nuevaCarta = 0;
         int puntuajeJugador = 0;
+        char deseo;
+        
 
         //Bienvenida
         System.out.println("Bienvenido al juego de Black Jack");
@@ -21,11 +23,33 @@ public class Backjack {
         System.out.println("Con un puntuaje total de: " + puntuajeJugador);
         //Se pregunta si va continuar
         System.out.println("Desea una carta más? S/N");
-        String deseo = sc.nextLine();
+        deseo = sc.nextLine().toUpperCase().charAt(0);
 
-        while (deseo == ) {
+        //Ciclo para seguir dando cartas
+        while (deseo == 'S' && puntuajeJugador < 21) {
             
+            // Se genera la nueva
+            nuevaCarta = (int)(Math.random()*10 + 1);
+            puntuajeJugador = puntuajeJugador + nuevaCarta;
+            System.out.println("Su nueva carta tiene un valor de : " + nuevaCarta);
+            System.out.println("Con un puntuaje total de: " + puntuajeJugador);
+
+            if (puntuajeJugador > 21){
+            System.out.println("Te pasaste de 21! Perdiste.");
+                System.out.println("Te pasaste por " + (puntuajeJugador - 21) + " puntos.");
+                deseo = 'N';
+
+            } else if (puntuajeJugador == 21) {
+                System.out.println("Felicidades! tienes un puntuaje de 21, black jack!!!");
+                deseo = 'N';
+
+            } else {
+                    System.out.println("¿Desea una carta más? S/N");
+                    deseo = sc.nextLine().toUpperCase().charAt(0);
+            }
         }
 
+
+        sc.close();
     }
 }
